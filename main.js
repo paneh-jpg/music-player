e899680f44d05eb5f57f5d9150457f8278011ede
paneh-jpg <vtpanh39.work@gmail.com>
2026-07-04 19:15:00 +0700

    ilmbwiwy

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// ==========================
-// Helpers
-// ==========================
+import { PLAYLISTS } from "./playlist.js";
+
+console.log(PLAYLISTS);
+
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
@@ -29,375 +30,13 @@ function formatTime(seconds) {
 
 $$(".sb-menu li").forEach((item) => item.classList.remove("active"));
 
-// ==========================
-// PLAYLIST DATA
-// ==========================
-const PLAYLISTS = {
-  phuongAnh: [
-    {
-      name: "Harleys In Hawaii",
-      singer: "Katy Perry",
-      path: "./assets/music/playlist-1/song1.mp3",
-      image: "./assets/img/playlist-1/song1.jpg",
-    },
-    {
-      name: "Kiss Me More (feat. SZA)",
-      singer: "Doja Cat, SZA",
-      path: "./assets/music/playlist-1/song2.mp3",
-      image: "./assets/img/playlist-1/song2.jpg",
-    },
-    {
-      name: "YAD",
-      singer: "Vanna Rainelle",
-      path: "./assets/music/playlist-1/song3.mp3",
-      image: "./assets/img/playlist-1/song3.jpg",
-    },
-    {
-      name: "Young And Beautiful",
-      singer: "Lana Del Rey",
-      path: "./assets/music/playlist-1/song4.mp3",
-      image: "./assets/img/playlist-1/song4.jpg",
-    },
-    {
-      name: "Play Date",
-      singer: "Melanie Martinez",
-      path: "./assets/music/playlist-1/song5.mp3",
-      image: "./assets/img/playlist-1/song5.jpg",
-    },
-    {
-      name: "Mind Games",
-      singer: "Sickick",
-      path: "./assets/music/playlist-1/song6.mp3",
-      image: "./assets/img/playlist-1/song6.jpg",
-    },
-    {
-      name: "Somewhere Only We Know",
-      singer: "Gustixa, rhianne",
-      path: "./assets/music/playlist-1/song7.mp3",
-      image: "./assets/img/playlist-1/song7.jpg",
-    },
-    {
-      name: "Shinunoga E-Wa",
-      singer: "Fujii Kaze",
-      path: "./assets/music/playlist-1/song8.mp3",
-      image: "./assets/img/playlist-1/song8.jpg",
-    },
-    {
-      name: "Heat Waves",
-      singer: "Glass Animals",
-      path: "./assets/music/playlist-1/song9.mp3",
-      image: "./assets/img/playlist-1/song9.jpg",
-    },
-    {
-      name: "Love Story",
-      singer: "Indila",
-      path: "./assets/music/playlist-1/song10.mp3",
-      image: "./assets/img/playlist-1/song10.jpg",
-    },
-    {
-      name: "Cinnamon Girl",
-      singer: "Lana Del Rey",
-      path: "./assets/music/playlist-1/song11.mp3",
-      image: "./assets/img/playlist-1/song11.jpg",
-    },
-    {
-      name: "Blank Space",
-      singer: "Taylor Swift",
-      path: "./assets/music/playlist-1/song12.mp3",
-      image: "./assets/img/playlist-1/song12.jpg",
-    },
-    {
-      name: "Apocalypse",
-      singer: "Cigarettes After Sex",
-      path: "./assets/music/playlist-1/song13.mp3",
-      image: "./assets/img/playlist-1/song13.jpg",
-    },
-    {
-      name: "Kill Bill",
-      singer: "SZA",
-      path: "./assets/music/playlist-1/song14.mp3",
-      image: "./assets/img/playlist-1/song14.jpg",
-    },
-    {
-      name: "Those Eyes",
-      singer: "New West",
-      path: "./assets/music/playlist-1/song15.mp3",
-      image: "./assets/img/playlist-1/song15.jpg",
-    },
-    {
-      name: "End of Beginning",
-      singer: "Djo",
-      path: "./assets/music/playlist-1/song16.mp3",
-      image: "./assets/img/playlist-1/song16.jpg",
-    },
-    {
-      name: "Afterthought",
-      singer: "Joji, BENEE",
-      path: "./assets/music/playlist-1/song17.mp3",
-      image: "./assets/img/playlist-1/song17.jpg",
-    },
-    {
-      name: "Heather",
-      singer: "Conan Gray",
-      path: "./assets/music/playlist-1/song18.mp3",
-      image: "./assets/img/playlist-1/song18.jpg",
-    },
-  ],
-  biTiNguyen: [
-    {
-      name: "Marvin Gaye",
-      singer: "Charlie Puth, Meghan Trainor",
-      path: "./assets/music/playlist-2/1.mp3",
-      image: "./assets/img/playlist-2/1.jpg",
-    },
-    {
-      name: "Radioactive",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/2.mp3",
-      image: "./assets/img/playlist-2/2.jpg",
-    },
-    {
-      name: "Thunder",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/3.mp3",
-      image: "./assets/img/playlist-2/3.jpg",
-    },
-    {
-      name: "Bad Liar",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/4.mp3",
-      image: "./assets/img/playlist-2/4.jpg",
-    },
-    {
-      name: "Believer",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/5.mp3",
-      image: "./assets/img/playlist-2/5.jpg",
-    },
-    {
-      name: "Demons",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/6.mp3",
-      image: "./assets/img/playlist-2/6.jpg",
-    },
-    {
-      name: "Natural",
-      singer: "Imagine Dragons",
-      path: "./assets/music/playlist-2/7.mp3",
-      image: "./assets/img/playlist-2/7.jpg",
-    },
-    {
-      name: "Take Me To Church",
-      singer: "Hozier",
-      path: "./assets/music/playlist-2/8.mp3",
-      image: "./assets/img/playlist-2/8.jpg",
-    },
-    {
-      name: "Too Sweet",
-      singer: "Hozier",
-      path: "./assets/music/playlist-2/9.mp3",
-      image: "./assets/img/playlist-2/9.jpg",
-    },
-    {
-      name: "One More Night",
-      singer: "Maroon 5",
-      path: "./assets/music/playlist-2/10.mp3",
-      image: "./assets/img/playlist-2/10.jpg",
-    },
-    {
-      name: "One Thing",
-      singer: "One Direction",
-      path: "./assets/music/playlist-2/11.mp3",
-      image: "./assets/img/playlist-2/11.jpg",
-    },
-    {
-      name: "What Makes You Beautiful",
-      singer: "One Direction",
-      path: "./assets/music/playlist-2/12.mp3",
-      image: "./assets/img/playlist-2/12.jpg",
-    },
-    {
-      name: "Payphone",
-      singer: "Maroon 5, Wiz Khalifa",
-      path: "./assets/music/playlist-2/13.mp3",
-      image: "./assets/img/playlist-2/13.jpg",
-    },
-    {
-      name: "Me And My Broken Heart",
-      singer: "Rixton",
-      path: "./assets/music/playlist-2/14.mp3",
-      image: "./assets/img/playlist-2/14.jpg",
-    },
-    {
-      name: "Stitches",
-      singer: "Shawn Mendes",
-      path: "./assets/music/playlist-2/15.mp3",
-      image: "./assets/img/playlist-2/15.jpg",
-    },
-    {
-      name: "Counting Stars",
-      singer: "OneRepublic",
-      path: "./assets/music/playlist-2/16.mp3",
-      image: "./assets/img/playlist-2/16.jpg",
-    },
-    {
-      name: "All About That Bass",
-      singer: "Meghan Trainor",
-      path: "./assets/music/playlist-2/17.mp3",
-      image: "./assets/img/playlist-2/17.jpg",
-    },
-    {
-      name: "TiK ToK",
-      singer: "Kesha",
-      path: "./assets/music/playlist-2/18.mp3",
-      image: "./assets/img/playlist-2/18.jpg",
-    },
-    {
-      name: "Hall of Fame (feat. will.i.am)",
-      singer: "The Script, will.i.am",
-      path: "./assets/music/playlist-2/19.mp3",
-      image: "./assets/img/playlist-2/19.jpg",
-    },
-    {
-      name: "HandClap",
-      singer: "Fitz and The Tantrums",
-      path: "./assets/music/playlist-2/20.mp3",
-      image: "./assets/img/playlist-2/20.jpg",
-    },
-    {
-      name: "Happy - From 'Despicable Me 2'",
-      singer: "Pharrell Williams",
-      path: "./assets/music/playlist-2/21.mp3",
-      image: "./assets/img/playlist-2/21.jpg",
-    },
-    {
-      name: "Twerk It Like Miley",
-      singer: "Brandon Beal, Christopher",
-      path: "./assets/music/playlist-2/22.mp3",
-      image: "./assets/img/playlist-2/22.jpg",
-    },
-    {
-      name: "The Fox (What Does the Fox Say?)",
-      singer: "Ylvis",
-      path: "./assets/music/playlist-2/23.mp3",
-      image: "./assets/img/playlist-2/23.jpg",
-    },
-    {
-      name: "High Hopes",
-      singer: "Panic! At The Disco",
-      path: "./assets/music/playlist-2/24.mp3",
-      image: "./assets/img/playlist-2/24.jpg",
-    },
-    {
-      name: "Great Balls Of Fire - Live",
-      singer: "Miles Teller",
-      path: "./assets/music/playlist-2/25.mp3",
-      image: "./assets/img/playlist-2/25.jpg",
-    },
-    {
-      name: "Beautiful Girls",
-      singer: "Sean Kingston",
-      path: "./assets/music/playlist-2/26.mp3",
-      image: "./assets/img/playlist-2/26.jpg",
-    },
-    {
-      name: "Young Dumb & Broke",
-      singer: "Khalid",
-      path: "./assets/music/playlist-2/27.mp3",
-      image: "./assets/img/playlist-2/27.jpg",
-    },
-    {
-      name: "In Case You Didn't Know",
-      singer: "Brett Young",
-      path: "./assets/music/playlist-2/28.mp3",
-      image: "./assets/img/playlist-2/28.jpg",
-    },
-  ],
-  playList3: [
-    {
-      name: "Vạn vật như muốn ta bên nhau",
-      singer: "RIO",
-      path: "./assets/music/playlist-3/1.mp3",
-      image: "./assets/img/playlist-3/1.jpg",
-    },
-    {
-      name: "Đợi",
-      singer: "52Hz (prod. RIO)",
-      path: "./assets/music/playlist-3/2.mp3",
-      image: "./assets/img/playlist-3/2.jpg",
-    },
-    {
-      name: "Dù cho mau về sau",
-      singer: "buitruonglinh",
-      path: "./assets/music/playlist-3/3.mp3",
-      image: "./assets/img/playlist-3/3.jpg",
-    },
-    {
-      name: "Sinh ra đã là thứ đối lập nhau",
-      singer: "Emcee L (Da LAB) ft. Badbies",
-      path: "./assets/music/playlist-3/4.mp3",
-      image: "./assets/img/playlist-3/4.jpg",
-    },
-    {
-      name: "Kho báu",
-      singer: "(S)TRONG Trọng Hiếu",
-      path: "./assets/music/playlist-3/5.mp3",
-      image: "./assets/img/playlist-3/5.jpg",
-    },
-    {
-      name: "Phép màu",
-      singer: "MAYDAYs ",
-      path: "./assets/music/playlist-3/6.mp3",
-      image: "./assets/img/playlist-3/6.jpg",
-    },
-    {
-      name: "Hẹn hò nhưng ko yêu",
-      singer: "WENDY THẢO X V2T MEDIA ",
-      path: "./assets/music/playlist-3/7.mp3",
-      image: "./assets/img/playlist-3/7.jpg",
-    },
-    {
-      name: "Anh đã chờ rất lâu",
-      singer: "Sixkie Dawgz",
-      path: "./assets/music/playlist-3/8.mp3",
-      image: "./assets/img/playlist-3/8.jpg",
-    },
-    {
-      name: "Mưa tháng sáu",
-      singer: "VĂN MAI HƯƠNG (feat. GREY D, TRUNG QUÂN)",
-      path: "./assets/music/playlist-3/9.mp3",
-      image: "./assets/img/playlist-3/9.jpg",
-    },
-    {
-      name: "Đưa em về nhà",
-      singer: "GREY D",
-      path: "./assets/music/playlist-3/10.mp3",
-      image: "./assets/img/playlist-3/10.jpg",
-    },
-    {
-      name: "Tháng tư là lời nói dối của em",
-      singer: "Hà Anh Tuấn ",
-      path: "./assets/music/playlist-3/11.mp3",
-      image: "./assets/img/playlist-3/11.jpg",
-    },
-    {
-      name: "Id 072019",
-      singer: "W/n - id 072019 | 3107 ft 267",
-      path: "./assets/music/playlist-3/12.mp3",
-      image: "./assets/img/playlist-3/12.jpg",
-    },
-  ],
-};
-
-// ==========================
 // APP
-// ==========================
 const app = {
   currentIndex: 0,
   isPlaying: false,
   isRepeat: false,
   isRandom: false,
-  songs: [...PLAYLISTS.biTiNguyen],
+  songs: [...PLAYLISTS.playList4],
 
   // Load bài hát hiện tại
   loadCurrentSong() {
@@ -427,7 +66,7 @@ const app = {
           <p class="author">${song.singer}</p>
         </div>
         <div class="option"><i class="fas fa-ellipsis-h"></i></div>
-      </div>`
+      </div>`,
       )
       .join("");
     playlistContainer.innerHTML = html;
@@ -456,9 +95,7 @@ const app = {
     this.render();
   },
 
-  // ==========================
   // EVENTS
-  // ==========================
   handleEvents() {
     const _this = this;
 
@@ -554,7 +191,7 @@ const app = {
       if (!li) return;
       const key = li.dataset.playlist;
       $$("#left-playlist .sb-item").forEach((i) =>
-        i.classList.remove("active")
+        i.classList.remove("active"),
       );
       li.classList.add("active");
       _this.songs = [...PLAYLISTS[key]];
